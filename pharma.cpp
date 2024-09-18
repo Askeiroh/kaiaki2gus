bac53f344aa0527e6495c94b809540ffb24e4d51
Gustavo L. <44736381+D4rkks@users.noreply.github.com>
2024-09-18 19:10:08 -0300

    Update pharma.cpp

--- pharma.cpp
+++ pharma.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h> //arquivo fontedo windows, se for linux é bits/stdc++.h
 
-using namespace std; //standard
+using namespace std; //standard, se nn por essa merda tem que ficar chamando o namespace antes das funções tipo: std::cout << "teste";, fica uma merda pra ler
 
 //função main (raiz do programa)
 int main(){
