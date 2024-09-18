2406c08c991beb7fb514a287f1b6202dcd615c57
Gustavo L. <44736381+D4rkks@users.noreply.github.com>
2024-09-18 18:54:43 -0300

    Update pharma.cpp
    template

--- pharma.cpp
+++ pharma.cpp
@@ -1,3 +1,9 @@
-#include <Windows.h>
-#include <>
+#include <Windows.h> //arquivo fontedo windows, se for linux é bits/stdc++.h
 
+using namespace std; //standard
+
+//função main (raiz do programa)
+int main(){
+
+  return 0;
+};
