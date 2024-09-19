1382852d81d338abc15465ec73e3fa9113e24778
Gustavo Lass <darksteamlass@gmail.com>
2024-09-19 00:44:54 -0300

    teste

--- pharma.cpp
+++ pharma.cpp
@@ -2,7 +2,7 @@
 
 using namespace std; //standard, se nn por essa merda tem que ficar chamando o namespace antes das funções tipo: std::cout << "teste";, fica uma merda pra ler
 
-//função main (raiz do programa)
+//função main (raiz do programa) //teste
 int main(){
 
   return 0;
