40424b8736c9d36abd49997c9d9df2cc0f80b4bf
kailanimaf <kkai.maffei@gmail.com>
2024-09-26 14:27:54 -0300

    coloquei um espaço no comentario

--- pharma.cpp
+++ pharma.cpp
@@ -1,4 +1,4 @@
-#include <Windows.h> //arquivo fontedo windows, se for linux é bits/stdc++.h
+#include <Windows.h> //arquivo fonte do windows, se for linux é bits/stdc++.h
 
 using namespace std; //standard, se nn por essa merda tem que ficar chamando o namespace antes das funções tipo: std::cout << "teste";, fica uma merda pra ler
 
