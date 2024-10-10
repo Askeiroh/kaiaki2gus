e023a58bc437843d081765f2aac9f545be33b002
D4rkks <darksteamlass@gmail.com>
2024-10-10 18:26:21 -0300

    codigo simplificado

--- pharma.cpp
+++ pharma.cpp
@@ -1,23 +1,148 @@
-#include <Windows.h> //arquivo fonte do windows, se for linux é bits/stdc++.h
-#include "socket_server.hpp"
+#include <iostream>
+#include <vector>
+#include <map>
 
-using namespace std; //standard, se nn por essa merda tem que ficar chamando o namespace antes das funções tipo: std::cout << "teste";, fica uma merda pra ler
+using namespace std;
 
-//função main (raiz do programa) //teste
-int main(){
+class Produto{
+    public:
+    string nome;
+    string tipoProduto;
+    string principio;
+    string formaAdministracao;
+    string tipoEmbalagem;
+    string quantidade;
+    string apresentacao;
+    string concentracao;
+    string fabricante;
+    string tipo;
+    string uso;
+    string dataFabricacao;
+    string dataVencimento;
+    string lote;
+    string descricao;
+    string bula;
+    string desconto;
+    string entradaSistema;
+    string fornecedor;
+    string classeRemedio;
+    string retemReceita;
 
-  Socket servidorSocket;
-
-  if(servidorSocket.startupServer() != 0){
-    return 1;
+  void insertDB(){
+    cout << "Nome:" << endl;
+    cin >> nome;
+    cout << "Tipo Produto: " << endl;
+    cin >> tipoProduto;
+    cout << "Principio: " << endl;
+    cin >> principio;
+    cout << "Forma de administracao: " << endl;
+    cin >> formaAdministracao;
+    cout << "Tipo embalagem: " << endl;
+    cin >> tipoEmbalagem;
+    cout << "Quantidade: " << endl;
+    cin >> quantidade;
+    cout << "Apresentação: " << endl;
+    cin >> apresentacao;
+    cout << "Concentração: " << endl;
+    cin >> concentracao;
+    cout << "Fabricante/Laboratorio: " << endl;
+    cin >> fabricante;
+    cout << "Tipo: " << endl;
+    cin >> tipo;
+    cout << "Uso: " << endl;
+    cin >> uso;
+    cout << "Data de fabricação: " << endl;
+    cin >> dataFabricacao;
+    cout << "Data de vencimento: " << endl;
+    cin >> dataVencimento;
+    cout << "Lote: " << endl;
+    cin >> lote;
+    cout << "Descrição: " << endl;
+    cin >> descricao;
+    cout << "Bula: " << endl;
+    cin >> bula;
+    cout << "Desconto: " << endl;
+    cin >> desconto;
+    cout << "Entrada no sistema: " << endl;
+    cin >> entradaSistema;
+    cout << "Fornecedor: " << endl;
+    cin >> fornecedor;
+    cout << "Classe do remédio: " << endl;
+    cin >> classeRemedio;
+    cout << "Retém receita: " << endl;
+    cin >> retemReceita;
   }
-
-  if(servidorSocket.acceptConnection() != 0){
-    return 1;
+  void printProduto(){
+        cout << "\nProduto registrado:" << endl;
+        cout << "Nome: " << nome << endl;
+        cout << "Tipo Produto: " << tipoProduto << endl;
+        cout << "Principio: " << principio << endl;
+        cout << "Forma de Administracao: " << formaAdministracao << endl;
+        cout << "Tipo de Embalagem: " << tipoEmbalagem << endl;
+        cout << "Quantidade: " << quantidade << endl;
+        cout << "Apresentacao: " << apresentacao << endl;
+        cout << "Concentracao: " << concentracao << endl;
+        cout << "Fabricante: " << fabricante << endl;
+        cout << "Tipo: " << tipo << endl;
+        cout << "Uso: " << uso << endl;
+        cout << "Data de Fabricação: " << dataFabricacao << endl;
+        cout << "Data de Vencimento: " << dataVencimento << endl;
+        cout << "Lote: " << lote << endl;
+        cout << "Descricao: " << descricao << endl;
+        cout << "Bula: " << bula << endl;
+        cout << "Desconto: " << desconto << endl;
+        cout << "Entrada no Sistema: " << entradaSistema << endl;
+        cout << "Fornecedor: " << fornecedor << endl;
+        cout << "Classe do Remédio: " << classeRemedio << endl;
+        cout << "Retém Receita: " << retemReceita << endl;
   }
-
-  servidorSocket.sendMessage("Teste");
-  servidorSocket.receiveMessage();
-  servidorSocket.stopServer();
-  return 0;
 };
+
+void registrarProduto(map<string, Produto>& estoque){
+    while(true){
+        Produto p;
+        p.insertDB();
+
+        estoque[p.nome] = p;
+
+        cout << "Deseja adicionar outro produto? (s/n): " << endl;
+        string resposta;
+        cin >> resposta;
+        if (resposta != "s" && resposta != "S") {
+            break;
+        }
+    }
+}
+void consultarProduto(map<string, Produto>& estoque){
+    cout << "Digite o nome do produto que deseja consultar: ";
+    string nome;
+    cin >> nome;
+
+    auto it = estoque.find(nome);
+    if (it != estoque.end()) {
+        it->second.printProduto();
+    } else {
+        cout << "Produto nao encontrado" << endl;
+    }
+}
+int main(){
+    int opcao = 0;
+    map<string, Produto> db;
+
+    while(true){
+        cout << "Escolha opcao\n 1- Registrar produto\n 2- Consultar produto" << endl;
+        int opcao = 0;
+        cin >> opcao;
+       switch(opcao){
+        case 1:
+        registrarProduto(db);
+        break;
+        case 2:
+        consultarProduto(db);
+        break;
+        default:
+        break;
+       }
+    }
+
+}
